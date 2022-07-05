9d2d12e44050014856da57def158120224f41dcf
PandharkarSayali <97429105+PandharkarSayali@users.noreply.github.com>
2022-07-05 12:54:20 +0530

    Update inactive-users.component.ts

--- src/app/inactive-users/inactive-users.component.ts
+++ src/app/inactive-users/inactive-users.component.ts
@@ -21,9 +21,7 @@ export class InactiveUsersComponent implements OnInit {
     console.log("abc");  
       console.log("abc");  
    console.log("abc");
-     console.log("abc");
-    console.log("abc");
-     console.log("abc");
+   
   }
 
 }
